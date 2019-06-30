fc06de794ee45031ed105b8f7b38c1e6a3a90289
Wolf <36646784+NE1W01F@users.noreply.github.com>
2019-06-30 22:13:23 +0000

    Create test.js

---
+++ test.js
@@ -0,0 +1,12 @@
+var getCookies = function(){
+  var pairs = document.cookie.split(";");
+  var cookies = {};
+  for (var i=0; i<pairs.length; i++){
+    var pair = pairs[i].split("=");
+    cookies[(pair[0]+'').trim()] = unescape(pair[1]);
+  }
+  return cookies;
+}
+
+var myCookies = getCookies();
+alert(myCookies.secret);
